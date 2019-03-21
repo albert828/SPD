adf84db57845aecf2b456c5fafb4e937d4ae39af
mtesluk <mateusz@tesluk.pl>
2019-03-21 23:30:26 +0100

    Cmax for neh

--- Lab2/NEH/main.cpp
+++ Lab2/NEH/main.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <algorithm>
 #include <stdint.h>
+#include <ctime>
 
 using namespace std;
 
@@ -22,6 +23,37 @@ struct sTask
         return (time > task.time); //odwrocenie sortowania
     }
 };
+
+int get_cmax(vector<vector<uint32_t>> vmachines, vector<uint32_t> sorted) {
+    vector<int> sum;
+    for(vector<int>::size_type i = 0; i != vmachines.size(); i++) {
+        sum.push_back(0);
+    }
+    for(vector<int>::size_type i = 0; i != sorted.size(); i++) {
+        sum[0] += vmachines[0][sorted[i]];
+        for(vector<int>::size_type j = 1; j != vmachines.size(); j++) {
+            if(sum[j] < sum[j-1]) sum[j] = sum[j-1];
+            sum[j] += vmachines[j][sorted[i]];
+        }
+    }
+    return sum.back();
+}
+
+int my_get_cmax(vector<vector<uint32_t>> vmachines) {
+    vector<int> sum;
+    for(vector<int>::size_type i = 0; i != vmachines[0].size(); i++) {
+        sum.push_back(0);
+    }
+    for(vector<int>::size_type i = 0; i != vmachines.size(); i++) {
+        sum[0] += vmachines[i][0];
+        for(vector<int>::size_type j = 1; j != vmachines[i].size(); j++) {
+            if(sum[j] < sum[j-1]) sum[j] = sum[j-1];
+            sum[j] += vmachines[i][j];
+        }
+    }
+    return sum.back();
+}
+
 
 sTask_Info read_file(const string &filename, vector<vector<uint32_t>> &vmachines) {
     sTask_Info info;
@@ -77,6 +109,58 @@ vector<uint32_t> sort_tasks(const vector<vector<uint32_t>> &vmachines, const sTa
     return task_index;
 }
 
+vector<vector<uint32_t>> slice(vector<vector<uint32_t>>  v, int start=0, int end=-1) {
+    int oldlen = v.size();
+    int newlen;
+
+    if (end == -1 or end >= oldlen){
+        newlen = oldlen-start;
+    } else {
+        newlen = end-start;
+    }
+
+    vector<vector<uint32_t>> nv(newlen);
+
+    for (int i=0; i<newlen; i++) {
+        nv[i] = v[start+i];
+    }
+    return nv;
+}
+
+int get_cmax_neh(const vector<vector<uint32_t>> vmachines, vector<uint32_t> sorted) {
+  
+  vector<vector<uint32_t>> my_vm;
+  for(auto i : sorted) {
+    vector<uint32_t> tmp;
+    for(vector<int>::size_type j = 0; j != vmachines.size(); j++)
+      tmp.push_back(vmachines[j][i]);
+    my_vm.push_back(tmp);
+  }
+  
+  
+  vector<vector<uint32_t>> base = slice(my_vm,0,1);
+  vector<vector<uint32_t>> tmp = slice(my_vm,0,1);
+  vector<vector<uint32_t>> tmp2 = slice(my_vm,0,1);
+  int glob_min=0;
+  for(int i=1;i<my_vm.size();i++) {
+    int min=10000000;
+    for(int j=0;j<i+1;j++) {
+      tmp.insert(tmp.begin()+j,my_vm[i]);
+      int v = my_get_cmax(tmp);
+      if(min>v) {
+	glob_min=v;
+	min = v;
+	base = tmp;
+      }
+      tmp = tmp2;
+    }
+    tmp2 = base;
+    tmp = base;
+  }
+  return glob_min;
+}
+
+
 int main() {
     vector<vector<uint32_t>> vmachines;
     string filename;
@@ -91,7 +175,12 @@ int main() {
     vector<uint32_t> sorted_tasks(info.number_of_tasks);
     sorted_tasks = sort_tasks(vmachines, info);
     for (auto i : sorted_tasks)
-        cout << i << ",";
-
+        cout << i << " ";
+    cout << endl;
+    double duration;
+    clock_t start = clock();
+    cout << "Cmax for NEH: " << get_cmax_neh(vmachines, sorted_tasks);
+    duration = (clock() - start ) / (double) CLOCKS_PER_SEC;
+    cout << "Time: " << duration;
     return 0;
 }
